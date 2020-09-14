c31eafebe4bfa4798e33dff84eac7c956f99edfa
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2020-09-14 09:49:34 +0800

    update

--- yuhaiin/update.sh
+++ yuhaiin/update.sh
@@ -5,6 +5,7 @@ wget https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts -O ad_wars_
 wget "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=adblock&showintro=0&mimetype=plaintext" -O pglyoyo.txt
 wget https://logroid.github.io/adaway-hosts/hosts_no_white.txt -O ja_ad
 
+# DIRECT&PROXY
 cat accelerated-domains.china.conf | sed $'s/\r$//' | sed 's/server=\//\*\./g' | sed 's/\/114\.114\.114\.114/ DIRECT/g' > yuhaiin.conf
 cat google.china.conf | sed $'s/\r$//' | sed 's/server=\//\*\./g' | sed 's/\/114\.114\.114\.114/ DIRECT/g' >> yuhaiin.conf
 cat apple.china.conf | sed $'s/\r$//' | sed 's/server=\//\*\./g' | sed 's/\/114\.114\.114\.114/ DIRECT/g' >> yuhaiin.conf
@@ -14,11 +15,11 @@ cat abroad.conf | sed 's/^/\*\./g' | sed 's/$/ PROXY/g' >> yuhaiin.conf
 cat abroad_ip.conf | sed 's/$/ PROXY/g' >> yuhaiin.conf
 
 # AD
-cat yuhaiin.conf > yuhaiin_ad.conf
-cat pglyoyo.txt | sed $'s/\r$//' | sed '/*/!d' |sed 's/$/ BLOCK/g' >> yuhaiin_ad.conf
+cat pglyoyo.txt | sed $'s/\r$//' | sed '/ *[Adblock] */d' | sed 's/$/ BLOCK/g' > yuhaiin_ad.conf
 cat ja_ad | sed $'s/\r$//' | sed 's/127.0.0.1 //g' | sed '/#/'d  | sed '/^\s*$/d'  |sed 's/$/ BLOCK/g' >> yuhaiin_ad.conf
 cat ad_wars_hosts | sed $'s/\r$//' | sed 's/127.0.0.1 //g' | sed '/#/'d | sed '1,2d' | sed '/^ *$/d' |sed 's/$/ BLOCK/g' >> yuhaiin_ad.conf
 cat cn_ad.conf  | sed $'s/\r$//' | sed '/#/'d  | sed '/^\s*$/d'  |sed 's/$/ BLOCK/g' >> yuhaiin_ad.conf
+cat yuhaiin.conf >> yuhaiin_ad.conf
 
 # custom
 cat custom.conf | sed '/#/'d  >> yuhaiin.conf
